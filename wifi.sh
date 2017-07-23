cee5075925d21aea909cc7fe36821568951112d4
muratcesmecioglu <muratcesmecioglu@gmail.com>
2017-07-23 20:42:00 +0300

    Create wifi.sh

---
+++ wifi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+WIFISSID=$1
+WIFIPASS=$2
+
+/sbin/ip link set up wlan0
+wpa_passphrase "$WIFISSID" "$WIFIPASS" > /etc/wpa_supplicant.conf
+
+dhclient -r
+pkill wpa_supplicant
+ifidown --force wlan0
+sleep 1
+wpa_supplicant -c /etc/wpa_supplicant.conf -i wlan0 -B
+sleep 1
+dhclient -nw
