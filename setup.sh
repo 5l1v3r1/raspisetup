a16d4eb353a11c6c873428285e51fe774deb4ff4
muratcesmecioglu <muratcesmecioglu@gmail.com>
2017-11-24 00:41:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #---Henüz Tamamlanmadı---
-
-apt-get -y install screen
-screen
-
-systemctl stop network-manager && systemctl disable network-manager
-apt-get -y install gparted tightvncserver ifplugd
-#vncserver :1
-
 dpkg -i raspi-config_20121028_all.deb 
 raspi-config --expand-rootfs
+
+apt-get -y install screen
+screen -dmS guncelleme
+screen -S murat -X stuff "apt-get -y install gparted tightvncserver ifplugd \napt-get -y update \napt-get -y upgrade \napt-get -y install mitmf \n"
+
+systemctl stop network-manager && systemctl disable network-manager
+#vncserver :1
 
 #nano /etc/network/interfaces
 echo "allow-hotplug wlan0" >> /etc/network/interfaces
@@ -23,10 +22,10 @@ cd re4son-kernel-builder
 # Bluetooth Üzerinden SSH Bağlantısı İçin
 nano /lib/systemd/system/bluetooth.service
 # Bul -> Değiştir
-ExecStart=/usr/libexec/bluetooth/bluetoothd -C
+# ExecStart=/usr/libexec/bluetooth/bluetoothd -C
 
-#### Deneme:
-sed -i 's/original/new/g' /lib/systemd/system/bluetooth.service
+#### Deneme yap:
+# sed -i 's/original/new/g' /lib/systemd/system/bluetooth.service
 
 #### Lazım bir komut :)
 #rfcomm watch hci0 1 getty rfcomm0 115200 vt100 -a root
@@ -41,8 +40,3 @@ echo > /etc/motd
 wget motdpi
 mv motdpi > /etc/motdpi
 echo "bash /etc/motdpi" /etc/profile
-
-# Programları Yükleme
-apt-get -y upgrade
-apt-get update
-apt-get -y install mitmf
