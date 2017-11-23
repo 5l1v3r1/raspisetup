7e28d4e1d8b287fb463c2fa67cac42d01a5bc376
muratcesmecioglu <muratcesmecioglu@gmail.com>
2017-11-23 22:36:11 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,68 @@
+#---Henüz Tamamlanmadı---
+
+apt-get update
+
+apt-get -y install screen
+
+screen
+
+apt-get -y upgrade
+
+systemctl stop network-manager && systemctl disable network-manager
+
+apt-get -y install gparted tightvncserver ifplugd
+
+# NC ile bağlanıp diski genişlet
+
+vncserver :1
+
+# Ethernet Kablosuz Wifi Bağlantısı İçin
+
+nano /etc/network/interfaces
+
+Altına Ekle
+
+allow-hotplug wlan0
+iface wlan0 inet dhcp
+wpa-conf /etc/wpa_supplicant.conf
+# Bluetooth Çalıştırmak İçin
+
+git clone https://github.com/Re4son/re4son-kernel-builder.git
+
+cd re4son-kernel-builder
+
+./install.sh
+
+# Bluetooth Üzerinden SSH Bağlantısı İçin
+
+nano /lib/systemd/system/bluetooth.service
+
+# Bul -> Değiştir
+
+ExecStart=/usr/libexec/bluetooth/bluetoothd -C
+rfcomm watch hci0 1 getty rfcomm0 115200 vt100 -a root
+
+[ ] Servis dosyası eklenecek
+
+# MOTD Değiştirme
+
+echo > /etc/motd
+
+nano /etc/ssh/sshd_config
+
+/#Bul -> Değiştir
+
+PrintLastLog no
+wget motdpi
+
+mv motdpi > /etc/motdpi
+
+rc.locale motdpi'yi ekle
+
+# Scriptleri İndirme
+
+wget wifi.sh
+
+# Programları Yükleme
+
+apt-get -y install mitmf
