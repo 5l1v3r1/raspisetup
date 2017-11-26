e6e2a9fdfef27e769e8ecc84a20dd9b9fcb39744
muratcesmecioglu <muratcesmecioglu@gmail.com>
2017-11-26 03:03:26 +0300

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -7,7 +7,7 @@ wpa_passphrase "$WIFISSID" "$WIFIPASS" > /etc/wpa_supplicant.conf
 
 dhclient -r wlan0
 pkill wpa_supplicant
-ifidown --force wlan0
+ifdown --force wlan0
 sleep 1
 wpa_supplicant -c /etc/wpa_supplicant.conf -i wlan0 -B
 sleep 1
