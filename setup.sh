d60e87204a04ed50aa70cbe364d06d5fca1a2404
muratcesmecioglu <muratcesmecioglu@gmail.com>
2018-08-15 02:23:20 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,16 +3,13 @@
 #screen -dmS guncelleme
 #screen -S guncelleme -X stuff "apt-get -y install gparted tightvncserver \n"
 #gparted gerekmeyebilir, parted ile yapilabiliyor. expand betigini guncelle
-apt-get -y install screen gparted tightvncserver
+apt-get -y install screen tightvncserver
 
 #systemctl stop network-manager && systemctl disable network-manager
 
 #VNC şifresini ayarlar
 vncpasswd -f <<< "123456" > /root/.vnc/passwd 
-
-
-echo "VNC Server şifresi ayarlandi, masaustune baglan, diski genislet"
-
+echo "VNC Server şifresi ayarlandi: 123456"
 read -p "Devam etmek icin bir tusa basin"
 
 
@@ -20,6 +17,8 @@ read -p "Devam etmek icin bir tusa basin"
 echo "allow-hotplug wlan0" >> /etc/network/interfaces
 echo "iface wlan0 inet dhcp" >> /etc/network/interfaces
 echo "wpa-conf /etc/wpa_supplicant.conf" >> /etc/network/interfaces
+echo "Wifi otomatik baglanmak icin ayarlandi"
+read -p "Devam etmek icin bir tusa basin"
 
 # Bluetooth Çalıştırmak İçin
 #git clone https://github.com/Re4son/re4son-kernel-builder.git
@@ -34,13 +33,12 @@ echo "wpa-conf /etc/wpa_supplicant.conf" >> /etc/network/interfaces
 #### Deneme yap:
 # sed -i 's/original/new/g' /lib/systemd/system/bluetooth.service
 
-#### Lazım bir komut :)
-#rfcomm watch hci0 1 getty rfcomm0 115200 vt100 -a root
 
-#Servisler
+
+#Servisleri yukle
 #cp bluetooth.service /etc/systemd/system/bluetooth.service
 #cp rfcomm.service /etc/systemd/system/rfcomm.service
-#cp vncserver.service /etc/systemd/system/vncserver.service
+cp vncserver.service /etc/systemd/system/vncserver.service
 
 # MOTD Değiştirme
 sed -i "s/PrintLastLog yes/PrintLastLog no/g" /etc/ssh/sshd_config
@@ -57,12 +55,9 @@ chmod +x /usr/bin/wifioff
 chmod +x /usr/bin/yardim
 
 #Masaustunu kapatma
-systemctl disable ligthdm
+#systemctl disable ligthdm
 
-screen -dmS guncelleme
-screen -S guncelleme -X stuff "apt-get -y update \napt-get -y upgrade \n"
-read -p "Arkaplanda guncelleme yapiliyor"
-
-#---------------
-# fdisk -l /dev/mmcblk0 | grep "Disk /dev/mmcblk0" | cut -d" " -f7
-
+#TEST ICIN KAPATILDI
+#screen -dmS guncelleme
+#screen -S guncelleme -X stuff "apt-get -y update \napt-get -y upgrade \n"
+#read -p "Arkaplanda guncelleme yapiliyor"
